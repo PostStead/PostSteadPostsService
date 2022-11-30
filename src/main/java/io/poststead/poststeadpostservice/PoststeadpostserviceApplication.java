cbef24c8d362576c09d3e673391c497367bc198f
Alecsandu <45858741+Alecsandu@users.noreply.github.com>
2022-11-30 23:00:02 +0200

    info: Project init commit

---
+++ src/main/java/io/poststead/poststeadpostservice/PoststeadpostserviceApplication.java
@@ -0,0 +1,13 @@
+package io.poststead.poststeadpostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoststeadpostserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoststeadpostserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/poststead/poststeadpostservice/PoststeadpostserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.poststead.poststeadpostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoststeadpostserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
